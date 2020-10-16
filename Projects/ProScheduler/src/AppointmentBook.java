c3f183125ea09f03fe62b90e4f5e1f10e1a38d5a
Mara Munoz <maramunozdev@gmail.com>
2020-10-15 18:05:08 -0700

    Update AppointmentBook.java
    Added delete functionality, deleted unnecessary lines of code, changed schema DB_URL

--- Projects/ProScheduler/src/AppointmentBook.java
+++ Projects/ProScheduler/src/AppointmentBook.java
@@ -22,7 +22,7 @@ public class AppointmentBook {
 	private long time;
 	private String fName;
 	private String lName;
-	private int index;
+	private static int index;
 	static Scanner sc = new Scanner(System.in);
 
 	static ArrayList<Appointment> appts = new ArrayList<>();
@@ -30,7 +30,7 @@ public class AppointmentBook {
 	String pattern = "yyyy-MM-dd";
 	SimpleDateFormat format = new SimpleDateFormat(pattern);
 
-	static final String DB_URL = "jdbc:mysql://127.0.0.1/greendragon";
+	static final String DB_URL = "jdbc:mysql://127.0.0.1/SchedulePro";
 	static final String USER = "root";
 	static final String PASS = "root";
 
@@ -56,6 +56,7 @@ public class AppointmentBook {
 			System.out.println("1. See all Appointments");
 			System.out.println("2. Create Appointment");
 			System.out.println("3. Search for Appointment");
+			System.out.println("4. Delete Appointment");
 
 			int choice = 0;
 			try {
@@ -89,41 +90,173 @@ public class AppointmentBook {
 				// Remove Appointment
 				removeAppointment();
 				break;
+			case 5:
+				// Add student
+				addStudent();
+				break;
+			case 6:
+				// Show all Appointments
+				ShowAppointments();
+				break;
+			case 7:
+				// Add Appointment
+				addAppointment();
+				break;
+			case 8:
+				// Edit Appointment
+				System.out.println(appts);
+				System.out.println("Enter the ID for the contact you would like to delete");
+				changeAppointment();
+				break;
+			case 9:
+				// Remove Appointment
+				removeAppointment();
+				break;
 			}
 
 		} while (true); // We will handle the exit in the loop
 	}
 
-	public static void addAppointment() {
-		try {
-			appts.add(new Appointment(fName, lName, date, day, time));
-
-		} catch (Exception e) {
-			System.out.println("Whoops! Try again..");
-			addAppointment();
-		}
+	private static void ShowAppointments() {
+		// TODO Auto-generated method stub
+		
 	}
 
+	private static void addStudent() {
+		
+	}
+
+	public static void addAppointment() {
+		
+		boolean addAnother = true;
+		
+		System.out.println("****************************");
+		System.out.println("*** ADD APPOINTMENT ***");
+		System.out.println("****************************");
+		
+		do {
+		try {
+			
+			
+			String sql = "INSERT INTO Appointment "
+					+ "(Appointment_ID, Lesson_type, Student_ID, Student_fname, "
+					+ "Student_lname, FL_instructor_ID, FL_fname, FL_lname, Plane_ID, Date_Time) VALUES (?,?,?,?,?,?,?,?,?,?)";
+
+			PreparedStatement stmt = connection.prepareStatement(sql);
+
+			System.out.println("Student First Name : ");
+			String name = sc.next();
+			System.out.println("Student Last Name : ");
+			String last_name = sc.next();
+			System.out.println("Student ID : ");
+			int StudID = sc.nextInt();
+			System.out.println("Flight Instructor First Name : ");
+			String FL_fname = sc.next();
+			System.out.println("Flight Instructor Last Name : ");
+			String FL_lname = sc.next();
+			System.out.println("Flight Instructor ID : ");
+			int FL_ID = sc.nextInt();
+			System.out.println("Lesson Type? (PPL || IR || ME) ");
+			String type = sc.next();
+			System.out.println("Plane ID : ");
+			int Plane_ID = sc.nextInt();
+			System.out.println("Appointment Date and Time : \nFormat : YYYY-MM-DD, HH:MM-HH:MM");
+			String dateTime = sc.next();
+			System.out.println("Add another Appointment? (Y/N)");
+			String ans = sc.next().toUpperCase();
+			if (ans.equals("N")) {
+				addAnother = false;
+			} else {
+				addAnother = true;
+			}
+
+		} catch (Exception e) {
+			System.out.println("Invalid input. Try again.");
+			addAppointment();
+		}
+		} while (addAnother == true);
+
+	}
+	
 	public static void removeAppointment() {
-		appts.remove(index);
+		boolean removeAnother = true;
+		try {
+			String sql = "SELECT Appointment_ID, Lesson_type, Student_ID, Student_fname, "
+					+ "Student_lname, FL_instructor_ID, FL_fname, FL_lname, "
+					+ "Plane_ID, Date_Time FROM Appointment";
+			System.out.println("Welcome!");
+			PreparedStatement stmt = connection.prepareStatement(sql);
+			ResultSet results = stmt.executeQuery();
+			while (results.next()) {	
+				System.out.print(results.getInt("Appointment_ID") + ") ");
+				System.out.print("\t" + results.getString("Lesson_type") + " | ");
+				System.out.print("\t" + results.getInt("Student_ID") + " | ");
+				System.out.print("\t" + results.getString("Student_fname") + " ");
+				System.out.print("\t" + results.getString("Student_lname") + " | ");
+				System.out.print("\t" + results.getInt("Plane_ID") + "|");
+				System.out.print("\t" + results.getString("Date_Time") + "\n");
+			}
+			System.out.println("Which Appointment do you want to delete [THERE IS NO UNDO]?");
+			int id = sc.nextInt();
+			// DELETE a record from the table
+			sql = "DELETE FROM Appointment WHERE Appointment_ID = ?";
+			stmt = connection.prepareStatement(sql);
+			stmt.setInt(1, id);
+			stmt.execute();
+		} catch (SQLException e) {
+			e.printStackTrace();
+			System.out.print("Oops, try again!");
+			removeAppointment();
+		}
 	}
 
 	public static void changeAppointment() {
 
 	}
+	
+	public static void viewAppointments() {
+		int counter = 1;
+		System.out.println("***********************");
+		System.out.println("*** DISPLAY APPOINTMENTS ***");
+		System.out.println("***********************");
+
+		try {
+
+			String sql = "SELECT Appointment_ID, Lesson_type, Student_ID, Student_fname, "
+					+ "Student_lname, FL_instructor_ID, FL_fname, FL_lname, "
+					+ "Plane_ID, Date_Time FROM Appointment ORDER BY Appointment_ID";
+			Statement statement = connection.createStatement();
+			ResultSet results = statement.executeQuery(sql);
+			System.out.println("Appointments");
+			System.out.println("==========");
+			while (results.next()) {
+				System.out.print(results.getInt("Appointment_ID") + ") ");
+				System.out.print(results.getString("Lesson_type") + " | ");
+				System.out.print(results.getInt("Student_ID") + " | ");
+				System.out.print(results.getString("Student_fname") + " ");
+				System.out.print(results.getString("Student_lname") + " | ");
+				System.out.print(results.getInt("Plane_ID") + "|");
+				System.out.print(results.getString("Date_Time") + "\n");			
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+			System.out.print("Oops, lets try again!");
+			menu();
+		}
+	}
 
 	public void deleteUser() {
-		// delete user in array list
+		
 
 	}
 
 	public void addUser() {
-		// add user
+		
 
 	}
 
 	public void editUser() {
-		// edit user
+		
 
 	}
 
